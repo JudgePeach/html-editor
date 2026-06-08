6739e39b1421f454023f3170f8fc62506e3c63e4
JudgePeach <yangyunlong0325@gmail.com>
2026-06-08 16:54:07 +0800

    Initial Commit

---
+++ html-editor/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  html-editor
+//
+//  Created by 杨云龙 on 2026/6/8.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
